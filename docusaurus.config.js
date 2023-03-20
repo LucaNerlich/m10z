f775217c4f2c683943bc46d6272eb3773418863c
Luca Nerlich <luca.nerlich@googlemail.com>
2023-03-20 13:49:52 +0100

    Add favicon

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,7 +8,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
     title: 'Mindestens 10 Zeichen',
     tagline: 'M10Z',
-    favicon: 'img/favicon.ico',
+    favicon: 'img/M10Z_Transparent.ico',
 
     // Set the production url of your site here
     url: 'https://m10z.vercel.app/',
