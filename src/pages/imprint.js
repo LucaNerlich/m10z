018203783a6537eca352a35a75025380f10a7c4a
Til Breidenbach <128494604+tilmobaxter@users.noreply.github.com>
2023-03-21 16:15:27 +0100

    Update imprint.js

--- src/pages/imprint.js
+++ src/pages/imprint.js
@@ -14,20 +14,20 @@ export default function Hello() {
                     margin: '0 auto',
                 }}>
 
-                <h1>Impressum</h1>
-                Angaben gemäß § 5 TMG
+                <p><h1>Impressum</h1>
+                Angaben gemäß § 5 TMG</p>
 
-                <h2>Kontakt</h2>
+                <p><h2>Kontakt</h2>
                 Tilman Breidenbach<br/>
                 Bussardweg 13<br/>
                 58579  Schalksmühle<br/>
                 Telefon: +49 (0) 2355 5046019<br/>
-                E-Mail:  tilbreidenbach(at)gmail(dot)com<br/>
+                E-Mail:  tilbreidenbach(at)gmail(dot)com<br/></p>
                 
-                <h2>Redaktionell verantwortlich</h2>
+                <p><h2>Redaktionell verantwortlich</h2>
                 Tilman Breidenbach<br/>
                 Bussardweg 13<br/>
-                58579  Schalksmühle<br/> 
+                58579  Schalksmühle<br/></p>
             </div>
         </Layout>
     );
