7bbf69086c6bb3f960ad3746f1b67b0165212711
Til Breidenbach <128494604+tilmobaxter@users.noreply.github.com>
2023-03-26 18:49:05 +0200

    Update hello.js

--- src/pages/hello.js
+++ src/pages/hello.js
@@ -15,14 +15,14 @@ export default function Hello() {
                 <p>
                  <h2 style="color:#F16012;">Was ist M10Z?</h2>
 
-<p>M10Z ist ein offener Kanal für Videospielcontent und das Drumherum.<br/>
+M10Z ist ein offener Kanal für Videospielcontent und das Drumherum.<br />
 
-Wir haben Spaß an kurzen oder langen Texten mit mindestens 10 Zeichen. <br/>  
-Wir produzieren Podcasts in (un-) regelmäßigen Abständen.<br/>  
-Wir lassen uns überraschen was noch kommt.<br/>
+Wir haben Spaß an kurzen oder langen Texten mit mindestens 10 Zeichen. <br />  
+Wir produzieren Podcasts in (un-) regelmäßigen Abständen.<br />  
+Wir lassen uns überraschen was noch kommt.<br />
 
 Alles aus Spaß am Medium und dem kreativen Prozess.   
-Unentgeltlich, unabhängig, ungezwungen.</p>
+Unentgeltlich, unabhängig, ungezwungen.
 
 <h2 style="color:#F16012;">Wer sind wir?</h2>
 
@@ -30,16 +30,16 @@ Bisher ein loses Kollektiv, das seine Anfänge im <a href="https://community.was
 
 <h2 style="color:#F16012;">Was macht ihr?</h2>
 
-Schaut euch hier auf unserem Blog um und genießt die Inhalte.<br/>  
+Schaut euch hier auf unserem Blog um und genießt die Inhalte.<br />  
 Wenn euch gefällt was wir machen, schreibt uns eine <a href="mailto:m10z@posteo.de">Email</a> oder kommt zu uns ins <a href="https://community.wasted.de/">Forum</a>.<br/>  
-Wenn ihr meint, das könnt ihr ebenso gut oder besser:<br/>  
+Wenn ihr meint, das könnt ihr ebenso gut oder besser:<br />  
 Mitmachen ist ausdrücklich erwünscht. Meldet euch.
 
 <h2 style="color:#F16012;">Wer darf mitmachen?</h2>
 
-Jede*r mit Lust an Themen die zu M10Z passen.<br/>  
-Du musst keine Erfahrung haben. <br/>  
-Dies darf gerne ein Versuch für Dich sein. <br/>
+Jede*r mit Lust an Themen die zu M10Z passen.<br />  
+Du musst keine Erfahrung haben. <br />  
+Dies darf gerne ein Versuch für Dich sein. <br />
 
 <h2 style="color:#F16012;">M10Z hat keinen Platz für:</h2>
 
