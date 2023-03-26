a5e56f2db1a40e79dd276ce6d4aca584d5663370
Til Breidenbach <128494604+tilmobaxter@users.noreply.github.com>
2023-03-26 18:46:54 +0200

    Update hello.js

--- src/pages/hello.js
+++ src/pages/hello.js
@@ -13,37 +13,38 @@ export default function Hello() {
                     fontSize: '20px',
                 }}>
                 <p>
-                  **Was ist M10Z?**
+                 <h2 style="color:#F16012;">Was ist M10Z?</h2>
 
-M10Z ist ein offener Kanal für Videospielcontent und das Drumherum.
+<p>M10Z ist ein offener Kanal für Videospielcontent und das Drumherum.<br/>
 
-Wir haben Spaß an kurzen oder langen Texten mit mindestens 10 Zeichen.   
-Wir produzieren Podcasts in (un-) regelmäßigen Abständen.  
-Wir lassen uns überraschen was noch kommt.
+Wir haben Spaß an kurzen oder langen Texten mit mindestens 10 Zeichen. <br/>  
+Wir produzieren Podcasts in (un-) regelmäßigen Abständen.<br/>  
+Wir lassen uns überraschen was noch kommt.<br/>
 
 Alles aus Spaß am Medium und dem kreativen Prozess.   
-Unentgeltlich, unabhängig, ungezwungen.
+Unentgeltlich, unabhängig, ungezwungen.</p>
 
-**Wer sind wir?**
+<h2 style="color:#F16012;">Wer sind wir?</h2>
 
-Bisher ein loses Kollektiv, das seine Anfänge im [Forum](https://community.wasted.de "Wasted Forum") von [WASTED.de](http://http://wasted.de "Wasted") genommen hat.
+Bisher ein loses Kollektiv, das seine Anfänge im <a href="https://community.wasted.de/">Forum</a> von <a href="https://wasted.de/">WASTED.de</a> genommen hat.
 
-**Was macht ihr?**
+<h2 style="color:#F16012;">Was macht ihr?</h2>
 
-Schaut euch hier auf unserem Blog um und genießt die Inhalte.  
-Wenn euch gefällt was wir machen, schreibt uns eine Email oder kommt zu uns ins [Forum](https://community.wasted.de "Wasted Forum").  
-Wenn ihr meint, das könnt ihr ebenso gut oder besser:  
+Schaut euch hier auf unserem Blog um und genießt die Inhalte.<br/>  
+Wenn euch gefällt was wir machen, schreibt uns eine <a href="mailto:m10z@posteo.de">Email</a> oder kommt zu uns ins <a href="https://community.wasted.de/">Forum</a>.<br/>  
+Wenn ihr meint, das könnt ihr ebenso gut oder besser:<br/>  
 Mitmachen ist ausdrücklich erwünscht. Meldet euch.
 
-**Wer darf mitmachen?**
+<h2 style="color:#F16012;">Wer darf mitmachen?</h2>
 
-Jede*r mit Lust an Themen die zu M10Z passen.  
-Du musst keine Erfahrung haben.  
-Dies darf gerne ein Versuch für Dich sein.
+Jede*r mit Lust an Themen die zu M10Z passen.<br/>  
+Du musst keine Erfahrung haben. <br/>  
+Dies darf gerne ein Versuch für Dich sein. <br/>
 
-**M10Z hat keinen Platz für:**
+<h2 style="color:#F16012;">M10Z hat keinen Platz für:</h2>
 
-Sexismus, Rassismus, Antisemitismus, Homo- und Transphobie, Klassismus, Ableismus  
+Sexismus, Rassismus, Antisemitismus, Homo- und Transphobie, Klassismus, Ableismus 
+
                 </p>
             </div>
         </Layout>
