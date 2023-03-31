77d8f13e373dbb55ddce1fae2f9e38f72713d72f
Luca Nerlich <luca.nerlich@googlemail.com>
2023-03-31 16:51:46 +0200

    Add 'copy to clipboard' feed links in footer (#42)
    * eject footer-link-item
    
    * add copy to clipboard links to footer

---
+++ src/theme/Footer/LinkItem/index.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import Link from '@docusaurus/Link'
+import useBaseUrl from '@docusaurus/useBaseUrl'
+import isInternalUrl from '@docusaurus/isInternalUrl'
+import IconExternalLink from '@theme/Icon/ExternalLink'
+
+export default function FooterLinkItem({item}) {
+    const {to, href, label, prependBaseUrlToHref, clipboard, ...props} = item
+    const toUrl = useBaseUrl(to)
+    const normalizedHref = useBaseUrl(href, {forcePrependBaseUrl: true})
+    return (
+        <>
+            {clipboard ?
+                <div style={{cursor: 'pointer'}} onClick={() => {
+                    navigator.clipboard.writeText(href)
+                }}>{label}</div>
+                :
+                <Link
+                    className='footer__link-item'
+                    {...(href
+                        ? {href: prependBaseUrlToHref ? normalizedHref : href}
+                        : {to: toUrl})}
+                    {...props}>
+                    {label}
+                    {href && !isInternalUrl(href) && <IconExternalLink />}
+                </Link>
+            }
+        </>
+    )
+}
