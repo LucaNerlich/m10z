d3662f670c3f1ab50e672f72360fd9460cefa1ba
Luca Nerlich <luca.nerlich@googlemail.com>
2023-05-10 18:31:50 +0200

    update img width

--- src/components/Author.tsx
+++ src/components/Author.tsx
@@ -11,11 +11,11 @@ export default function Author(props: AuthorProps): JSX.Element {
     return (
         <div className={styles.author}>
             <h2 className={styles.headline}>{props.author.name}</h2>
-            <img src={props.author.image_url} alt={props.author.name} width='200px' />
+            <img className={styles.image} src={props.author.image_url} alt={props.author.name} />
+            <a href={props.author.url}>Alle Posts</a>
             {props.author?.title &&
                 <em>{props.author?.title}</em>
             }
-            <a href={props.author.url}>Alle Posts</a>
         </div>
     )
 }
