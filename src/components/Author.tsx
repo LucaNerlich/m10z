814ff74b7c9781e791d75da16c0320e7018e0e6c
Luca Nerlich <luca.nerlich@googlemail.com>
2023-05-12 17:19:08 +0200

    update author card design

--- src/components/Author.tsx
+++ src/components/Author.tsx
@@ -12,7 +12,9 @@ export default function Author(props: AuthorProps): JSX.Element {
         <div className={styles.author}>
             <div className={styles.wrapper}>
                 <h2 className={styles.headline}>{props.author.name}</h2>
-                <img className={styles.image} src={props.author.image_url} alt={props.author.name} />
+                {props.author.image_url &&
+                    <img className={styles.image} src={props.author.image_url} alt={props.author.name} />
+                }
                 <em>{props.author.id}</em>
                 <hr />
                 <a className={styles.link} href={props.author.url}>Alle Posts 🔗</a>
