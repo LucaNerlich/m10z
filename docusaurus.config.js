0f148734758071ef598f98bd134cde5cad30ba7b
Luca Nerlich <luca.nerlich@googlemail.com>
2024-01-08 10:00:29 +0100

    upgrade docusaurus

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -17,7 +17,7 @@ const config = {
     organizationName: 'LucaNerlich', // Usually your GitHub org/user name.
     projectName: 'm10z', // Usually your repo name.
 
-    onBrokenLinks: 'throw',
+    onBrokenLinks: 'warn',
     onBrokenMarkdownLinks: 'warn',
 
     // Even if you don't use internalization, you can use this field to set useful
