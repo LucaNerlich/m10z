95fd7a93d82d08bfc713a75269ea49a5b3590a49
Luca Nerlich <luca.nerlich@googlemail.com>
2024-01-16 17:25:52 +0100

    Update generateAudioFeed.js

--- generateAudioFeed.js
+++ generateAudioFeed.js
@@ -1,22 +1,45 @@
 const fs = require('fs');
-const util = require('util');
+const yaml = require('js-yaml');
+const xml2js = require('xml2js');
 
-// Convert fs.readFile into Promise version to use with async/await.
-const readFile = util.promisify(fs.readFile);
+function yamlObjectToXml(yamlObject) {
+    const xmlObject = {
+        'item': {
+            'title': yamlObject.title,
+            'pubDate': new Date(yamlObject.date).toUTCString(),
+            'guid': {
+                _: 'ein-hash22',
+                $: {isPermaLink: 'false'},
+            },
+            'itunes:image': {
+                'url': yamlObject.image,
+                'title': `${yamlObject.title} (m10z)`,
+                'link': 'https://m10z.de',
+            },
+            'description': yamlObject.description,
+            'author': 'm10z@posteo.de',
+            'itunes:duration': yamlObject.duration,
+            'enclosure': {
+                $: {
+                    url: yamlObject.url,
+                    length: '48300000',
+                    type: 'audio/mpeg',
+                },
+            },
+        },
+    };
 
-async function run() {
-    console.log('Converting audio.yaml to audiofeed.xml');
-
-    // Specify the path to your XML file
-    const xmlPath = './templates/rss-channel.xml';
-
-    try {
-        const data = await readFile(xmlPath, 'utf-8');
-        console.log('XML file has been read successfully');
-        console.log(data);
-    } catch (error) {
-        console.error('Error reading XML file: ', error);
-    }
+    const builder = new xml2js.Builder({headless: true}); // headless option to remove XML declaration
+    return builder.buildObject(xmlObject);
 }
 
-run();
+const basepath = './static/audiofeed';
+let yamlData = fs.readFileSync(basepath + '.yaml', 'utf8');
+let yamlObjects = yaml.load(yamlData);
+
+let convertedItems = [];
+for (let rssItem in yamlObjects) {
+    convertedItems.push(yamlObjectToXml(rssItem));
+}
+
+console.log('convertedItems', convertedItems);
