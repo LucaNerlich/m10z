7edb63562ffd515a205cfe6fc8c1273886207ac6
Luca Nerlich <nerlich@adobe.com>
2024-11-14 12:16:19 +0100

    Switch from Plausible to Umami tracking
    Replaced Plausible tracking script with Umami tracking script in plausible.js. Updated the script source URL and the data-website-id attribute accordingly.

--- plausible.js
+++ plausible.js
@@ -8,26 +8,12 @@ import ExecutionEnvironment from '@docusaurus/ExecutionEnvironment'
 
 // https://stackoverflow.com/questions/57859350/how-can-i-add-custom-scripts-in-index-htmls-head-part-in-docusaurus-v2
 if (ExecutionEnvironment.canUseDOM) {
-
-    window.plausible = window.plausible || function() {
-        (window.plausible.q = window.plausible.q || []).push(arguments)
-    }
-
-    // Create the script element for the Clarity tracking code
-
-    plausibleScript.src = 'https://plausible-m10z.lucanerlich.com/js/script.file-downloads.outbound-links.js'
-    plausibleScript.async = false
-    plausibleScript.defer = true
-    plausibleScript.setAttribute('data-domain', 'm10z.lucanerlich.com')
-
-    // Insert the script into the head of the document
-    document.head.appendChild(plausibleScript)
-
+    // Create the script element for the umami tracking code
     const umamiScript = document.createElement('script')
-    umamiScript.src = 'https://cloud.umami.is/script.js'
+    umamiScript.src = 'https://umami-t8kgsg4o4wc4o80wgwwo484c.lucanerlich.com/script.js'
     umamiScript.async = false
     umamiScript.defer = true
-    umamiScript.setAttribute('data-website-id', 'd0d4c4ba-3a09-4585-b94b-21e11dc01b2c')
+    umamiScript.setAttribute('data-website-id', 'ff848c61-f514-41d1-a139-def9da0ee3c1')
 
     // Insert the script into the head of the document
     document.head.appendChild(umamiScript)
