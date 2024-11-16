52edc92517401dece6782cd69f2b17c0f8fadc40
Luca Nerlich <luca.nerlich@googlemail.com>
2024-11-16 10:40:44 +0100

    Prevent Umami tracking on localhost
    Refactor script inclusion to skip Umami tracking if the site is accessed from localhost. This ensures that development environments do not send tracking data, improving privacy and debugging performance.

--- umami.js
+++ umami.js
@@ -1,14 +1,18 @@
-import ExecutionEnvironment from '@docusaurus/ExecutionEnvironment'
+import ExecutionEnvironment from '@docusaurus/ExecutionEnvironment';
 
 // https://stackoverflow.com/questions/57859350/how-can-i-add-custom-scripts-in-index-htmls-head-part-in-docusaurus-v2
 if (ExecutionEnvironment.canUseDOM) {
-    // Create the script element for the umami tracking code
-    const umamiScript = document.createElement('script')
-    umamiScript.src = 'https://umami-t8kgsg4o4wc4o80wgwwo484c.lucanerlich.com/script.js'
-    umamiScript.async = false
-    umamiScript.defer = true
-    umamiScript.setAttribute('data-website-id', 'ff848c61-f514-41d1-a139-def9da0ee3c1')
+    if (window.location.href.includes('localhost')) {
+        console.debug('Skipping umami tracking on localhost');
+    } else {
+        // Create the script element for the umami tracking code
+        const umamiScript = document.createElement('script');
+        umamiScript.src = 'https://umami-t8kgsg4o4wc4o80wgwwo484c.lucanerlich.com/script.js';
+        umamiScript.async = false;
+        umamiScript.defer = true;
+        umamiScript.setAttribute('data-website-id', 'ff848c61-f514-41d1-a139-def9da0ee3c1');
 
-    // Insert the script into the head of the document
-    document.head.appendChild(umamiScript)
+        // Insert the script into the head of the document
+        document.head.appendChild(umamiScript);
+    }
 }
