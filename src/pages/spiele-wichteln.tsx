7c569a4cc763c4eae7709dcd036c56e3bfe837b0
Luca Nerlich <luca.nerlich@googlemail.com>
2025-01-08 17:06:31 +0100

    init empty page

---
+++ src/pages/spiele-wichteln.tsx
@@ -0,0 +1,13 @@
+'use client';
+import React from 'react';
+
+interface spielewichtelnProps {
+}
+
+export default function spieleWichteln(props: Readonly<spielewichtelnProps>): React.ReactElement {
+    return (
+        <>
+            <p>I am a spieleWichteln</p>
+        </>
+    );
+}
