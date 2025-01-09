f3ebab51dfa7a296f5e9d58a3aeb7060052a9e5a
Luca Nerlich <luca.nerlich@googlemail.com>
2025-01-09 10:16:51 +0100

    Add form and pairing logic to Spiele-Wichteln page
    Introduces a button for generating pairs and displays results dynamically. Created a `pair` interface and placeholder `handleSubmit` function to handle pair generation logic. Updated layout with headings and placeholders for export formats.

--- src/pages/spiele-wichteln.tsx
+++ src/pages/spiele-wichteln.tsx
@@ -11,12 +11,38 @@ interface participant {
     link: string; // main gaming platform, such as steam profile page
 }
 
+interface pair {
+    sender: participant;
+    reciever: participant;
+}
+
 export default function spieleWichteln(props: Readonly<spielewichtelnProps>): React.ReactElement {
+    const [results, setResults] = React.useState<pair[]>([]);
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        console.log('generate pairs');
+    }
+
     return (
         <Layout title='Spiele-Wichteln' description='Web App zum Auslosen von Wichteln Paaren'>
-            <div className="wrapper">
-                <p>I am a spieleWichteln</p>
+            <div className='wrapper'>
+                <h1>Spiele-Wichteln Auslosung</h1>
+                <form onSubmit={handleSubmit}>
+                    <button type='submit'>Paare auslosen</button>
+                </form>
+
+                <h2>Kopiervorlage <strong>Discourse</strong></h2>
+                <h2>Kopiervorlage <strong>phpbb</strong></h2>
+                <h2>Kopiervorlage <strong>Text</strong></h2>
+                {results.map((pair, index) =>
+                    <p key={index}>
+                        <a href={pair.sender.link} target='_blank'>{pair.sender.name}</a>
+                        <strong>bewichtelt ➡️</strong>
+                        <a href={pair.reciever.link} target='_blank'>{pair.reciever.name}</a>
+                    </p>,
+                )}
             </div>
         </Layout>
-);
+    );
 }
