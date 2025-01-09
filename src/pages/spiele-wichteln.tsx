3eff0b8077cb0d339e81d0c71a491e9be5697ad4
Luca Nerlich <luca.nerlich@googlemail.com>
2025-01-09 10:24:08 +0100

    Initialize demo participants and pairs for Secret Santa.
    Added a default list of participants and corresponding demo pairs for easier development and testing. Updated the useState variables and adjusted rendering to reflect the new pairs structure.

--- src/pages/spiele-wichteln.tsx
+++ src/pages/spiele-wichteln.tsx
@@ -17,11 +17,29 @@ interface pair {
 }
 
 export default function spieleWichteln(props: Readonly<spielewichtelnProps>): React.ReactElement {
-    const [results, setResults] = React.useState<pair[]>([]);
+    // set demo participants
+    const [participants, setParticipants] = React.useState<participant[]>([
+        {name: 'Player 1', link: 'https://steamcommunity.com/id/player1'},
+        {name: 'Player 2', link: 'https://steamcommunity.com/id/player2'},
+        {name: 'Player 3', link: 'https://steamcommunity.com/id/player3'},
+        {name: 'Player 4', link: 'https://steamcommunity.com/id/player4'},
+        {name: 'Player 5', link: 'https://steamcommunity.com/id/player5'},
+    ]);
+    const [pairs, setPairs] = React.useState<pair[]>([]);
+
 
     function handleSubmit(event) {
-        event.preventDefault();
         console.log('generate pairs');
+        event.preventDefault();
+
+        // set demo pairs
+        setPairs([
+            {sender: participants[0], receiver: participants[1]},
+            {sender: participants[1], receiver: participants[2]},
+            {sender: participants[2], receiver: participants[3]},
+            {sender: participants[3], receiver: participants[4]},
+            {sender: participants[4], receiver: participants[0]},
+        ]);
     }
 
     return (
@@ -35,10 +53,10 @@ export default function spieleWichteln(props: Readonly<spielewichtelnProps>): Re
                 <h2>Kopiervorlage <strong>Discourse</strong></h2>
                 <h2>Kopiervorlage <strong>phpbb</strong></h2>
                 <h2>Kopiervorlage <strong>Text</strong></h2>
-                {results.map((pair, index) =>
+                {pairs.map((pair, index) =>
                     <p key={index}>
                         <a href={pair.sender.link} target='_blank'>{pair.sender.name}</a>
-                        <strong>bewichtelt ➡️</strong>
+                        <strong>&nbsp;bewichtelt ➡️&nbsp;</strong>
                         <a href={pair.receiver.link} target='_blank'>{pair.receiver.name}</a>
                     </p>,
                 )}
