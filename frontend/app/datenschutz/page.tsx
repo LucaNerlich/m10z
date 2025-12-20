a75b081ee9b6287880c09043ac55d29bd7524780
Luca Nerlich <nerlich@adobe.com>
2025-12-20 17:53:08 +0100

    get and parse single type markdown to legal pages

--- frontend/app/datenschutz/page.tsx
+++ frontend/app/datenschutz/page.tsx
@@ -1,7 +1,18 @@
-export default function PrivacyPage() {
-    return (
-        <main>
-            <h1>Datenschutz</h1>
-        </main>
-    );
+import { Markdown } from '@/src/lib/markdown/Markdown';
+import { getPrivacy } from '@/src/lib/strapi';
+
+export const revalidate = 3600;
+
+export default async function PrivacyPage() {
+  const privacy = await getPrivacy({
+    revalidateSeconds: revalidate,
+    tags: ['legal', 'privacy'],
+  });
+
+  return (
+    <main>
+      <h1>{privacy.title}</h1>
+      <Markdown markdown={privacy.content} />
+    </main>
+  );
 }
--- frontend/app/impressum/page.tsx
+++ frontend/app/impressum/page.tsx
@@ -1,7 +1,18 @@
-export default function ImprintPage() {
-    return (
-        <main>
-            <h1>Impressum</h1>
-        </main>
-    );
+import { Markdown } from '@/src/lib/markdown/Markdown';
+import { getImprint } from '@/src/lib/strapi';
+
+export const revalidate = 3600;
+
+export default async function ImprintPage() {
+  const imprint = await getImprint({
+    revalidateSeconds: revalidate,
+    tags: ['legal', 'imprint'],
+  });
+
+  return (
+    <main>
+      <h1>{imprint.title}</h1>
+      <Markdown markdown={imprint.content} />
+    </main>
+  );
 }
