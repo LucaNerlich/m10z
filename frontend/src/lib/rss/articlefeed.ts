f9d4f704c4b118676ddb595ac080644afbfd5c2d
Luca Nerlich <nerlich@adobe.com>
2025-12-23 22:54:06 +0100

    update default channel image URL in article feed

--- frontend/src/lib/rss/articlefeed.ts
+++ frontend/src/lib/rss/articlefeed.ts
@@ -59,7 +59,7 @@ export function generateArticleFeedXml(args: {
     const channelImage = normalizeStrapiMedia(channel.image);
     const channelImageUrl =
         mediaUrlToAbsolute({media: channelImage}) ??
-        `${siteUrl}/static/img/formate/cover/m10z.jpg`;
+        `${siteUrl}/images/m10z.jpg`;
     const header =
         `<?xml version="1.0" encoding="UTF-8"?>` +
         `<rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom" xmlns:content="http://purl.org/rss/1.0/modules/content/">` +
