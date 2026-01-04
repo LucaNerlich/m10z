78eff1a063153ee95d5084028c102fbdb2578dcb
Luca Nerlich <luca.nerlich@gmail.com>
2026-01-04 19:40:27 +0100

    add image.qualities

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -28,6 +28,7 @@ const nextConfig: NextConfig = {
         remotePatterns: getRemotePatterns(),
         deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
         imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+        qualities: [50, 60, 75, 80, 90, 95]
     },
     async redirects() {
         return [
