18089227add994bfcc3fc8e64096ad4a92582ca1
Luca Nerlich <nerlich@adobe.com>
2026-08-27 11:11:47 +0200

    use new media library

---
+++ backend/config/features.ts
@@ -0,0 +1,7 @@
+export default () => ({
+    future: {
+        // https://strapi.notion.site/Media-Library-Beta-Release-3c78f3598074810dbad6f2addfa25b6f
+        // https://docs.strapi.io/cms/features/media-library
+        betaMediaLibrary: true,
+    },
+});
